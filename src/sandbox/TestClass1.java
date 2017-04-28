ac58812c54d16544fbba297573d07fc8112d5ab3
Mark Bernard <markbernard@users.noreply.github.com>
2017-04-28 10:46:44 -0400

    Update TestClass1.java

--- src/sandbox/TestClass1.java
+++ src/sandbox/TestClass1.java
@@ -3,10 +3,5 @@ package sandbox;
 public class TestClass1 {
     public static void main(String... args) {
         System.out.println("Testing Git");
-        System.out.println("Other change.");
-    }
-    
-    public void addMethod() {
-    	System.out.println("Change to add method.");
     }
 }
