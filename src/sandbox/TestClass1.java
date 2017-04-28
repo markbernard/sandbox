9ab6e4eeee891599290baaeecc0dcd697a81eff1
Mark Bernard <markbernard@users.noreply.github.com>
2017-04-28 14:19:38 -0400

    Update TestClass1.java

--- src/sandbox/TestClass1.java
+++ src/sandbox/TestClass1.java
@@ -3,20 +3,5 @@ package sandbox;
 public class TestClass1 {
     public static void main(String... args) {
         System.out.println("Testing Git");
-        System.out.println("Some changes");
-        System.out.println("Other change");
-        System.out.println("Remote change");
-        System.out.println("Another remote change");
-        System.out.println("One more local change");
-        System.out.println("One more remote change");
-        System.out.println("Remote change");
-    }
-    
-    public void addMethod() {
-    	System.out.println("New Git method");
-    }
-    
-    public void addAnotherMethod() {
-    	System.out.println("Another new Git method");
     }
 }
