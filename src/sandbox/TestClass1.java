020ee9c1bc96e121d2a25d713928ba28498e8bae
Mark Bernard <markbernard@users.noreply.github.com>
2017-04-28 13:16:36 -0400

    Update TestClass1.java

--- src/sandbox/TestClass1.java
+++ src/sandbox/TestClass1.java
@@ -3,5 +3,6 @@ package sandbox;
 public class TestClass1 {
     public static void main(String... args) {
         System.out.println("Testing Git");
+        System.out.println("Other change");
     }
 }
