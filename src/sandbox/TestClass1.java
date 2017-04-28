5e0cd604e0050ae779a2503f0b642a0ac2d1b655
markbernard <mark.a.bernard@gmail.com>
2017-04-28 10:42:19 -0400

    Changes

--- src/sandbox/TestClass1.java
+++ src/sandbox/TestClass1.java
@@ -4,4 +4,8 @@ public class TestClass1 {
     public static void main(String... args) {
         System.out.println("Testing Git");
     }
+    
+    public void addMethod() {
+    	System.out.println("Change to add method.");
+    }
 }
