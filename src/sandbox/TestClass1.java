bdf890e690e61a3a91b9c3c23ed744a4fb556248
markbernard <mark.a.bernard@gmail.com>
2017-05-01 10:46:56 -0400

    Test branch commit.

--- src/sandbox/TestClass1.java
+++ src/sandbox/TestClass1.java
@@ -3,7 +3,5 @@ package sandbox;
 public class TestClass1 {
     public static void main(String... args) {
         System.out.println("Testing Git");
-        System.out.println("Some changes");
-        System.out.println("Other changes");
     }
 }
